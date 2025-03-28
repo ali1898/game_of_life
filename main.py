c9a30f6a84f2440be5150e203a4a21c04e118edf
AliAhmadiNasab <aliahmadinasab81@gmail.com>
2025-03-28 12:03:03 +0330

    Starting & Stopping the Simulation

--- main.py
+++ main.py
@@ -22,9 +22,29 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                simulation.start()
+                pygame.display.set_caption("Game of Life is running")
+            elif event.key == pygame.K_SPACE:
+                simulation.stop()
+                pygame.display.set_caption("Game of Life has stopped")
+            elif event.key == pygame.K_f:
+                FPS += 2
+            elif event.key == pygame.K_s:
+                if FPS > 5:
+                    FPS -= 2
+            elif event.key == pygame.K_r:
+                simulation.create_random_state()
+            elif event.key == pygame.K_c:
+                simulation.clear()
+            elif event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
     # Updating State
     simulation.update()
-    
+
     # Drawing
     window.fill(GREY)
     simulation.draw(window)
